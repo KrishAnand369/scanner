18ad175c0105be059fe0143a6f546369fb944f9c
KrishAnand369 <126272210+KrishAnand369@users.noreply.github.com>
2023-03-11 15:08:03 +0530

    Create main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: QRScanner(),
+      home: HomePage(),
       theme: ThemeData(
           appBarTheme: AppBarTheme(backgroundColor: Colors.tealAccent)),
     );
